4fff68d37c1f2dbd558c29ec5f2bd8ed89d43187
PaulHaze <dev@artemist.com.au>
2023-11-02 07:10:52 +1100

    add search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { LoginForm } from '@/app/ui/login-form';
+import { Search } from '@/app/ui/search';
 
 export default function Home() {
   return (
-    <div className="flex-center h-screen flex-col bg-gray-300">
+    <div className="h-screen bg-gray-300 p-5">
+      <Search placeholder="search" />
       <LoginForm />
     </div>
   );

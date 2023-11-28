2e4de2f85ba147c29079aaae830779d06f842a89
PaulHaze <dev@artemist.com.au>
2023-11-28 12:26:42 +1100

    add dashboard elements

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,42 @@
 import { Card, RevenueChart, LatestInvoices } from '@/components/dashboard';
 
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/lib/data';
+
 import { lusitana } from '@/components/ui';
 
-export default function DashboardPage() {
+/* 
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices,
+*/
+
+export default async function DashboardPage() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 `}>Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        /> */}
-        Cards Placeholder
+        />
       </div>
       <div className="grid-grid-cols-1 mt-6 gap-6 md:grid-cols-5 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
-        Revenue Placeholder
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );

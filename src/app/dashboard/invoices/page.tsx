e393ca91b8ed8455dbadb5929a7d0dd8bb134af0
PaulHaze <dev@artemist.com.au>
2023-12-05 17:09:49 +1100

    add imports for invoices page

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,3 +1,11 @@
+import { Suspense } from 'react';
+import {
+  Pagination,
+  InvoicesTable,
+  CreateInvoice,
+} from '@/components/invoices';
+import { Search, lusitana, InvoicesTableSkeleton } from '@/components/ui';
+
 export default function InvoicesPage() {
   return (
     <div>
--- src/components/invoices/index.ts
+++ src/components/invoices/index.ts
@@ -2,4 +2,5 @@ export * from './Breadcrumbs';
 export * from './CreateForm';
 export * from './EditInvoiceForm';
 export * from './InvoiceButtons';
+export * from './InvoicesTable';
 export * from './Pagination';

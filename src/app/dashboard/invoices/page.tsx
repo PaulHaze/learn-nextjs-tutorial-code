b9b610ea58e67147643ef992638b01e8e04b9309
PaulHaze <dev@artemist.com.au>
2023-12-05 17:25:20 +1100

    add search to URL feature

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from 'react';
+
 import {
   Pagination,
   InvoicesTable,
@@ -6,10 +7,22 @@ import {
 } from '@/components/invoices';
 import { Search, lusitana, InvoicesTableSkeleton } from '@/components/ui';
 
-export default function InvoicesPage() {
+export default async function InvoicesPage() {
   return (
-    <div>
-      <p>InvoicesPage Placeholder</p>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
     </div>
   );
 }
--- src/components/ui/Search.tsx
+++ src/components/ui/Search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 type SearchProps = {
@@ -7,6 +8,20 @@ type SearchProps = {
 };
 
 export function Search({ placeholder }: SearchProps) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -16,6 +31,10 @@ export function Search({ placeholder }: SearchProps) {
         type="text"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
